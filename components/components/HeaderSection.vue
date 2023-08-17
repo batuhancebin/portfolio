8dfa703aaa95899a3ef5cbcd14483cfee5bcec69
Batuhan Çebin <63672094+batuhancebin@users.noreply.github.com>
2023-08-17 12:28:43 +0300

    changed header

---
+++ components/components/HeaderSection.vue
@@ -0,0 +1,54 @@
+<template>
+    <section>
+        <header>
+            <h1 id="my-text">Oktay Kaypak</h1>
+            <h1 id="my-text" class="bussines">UI & UX Developer</h1>
+        </header>
+    </section>
+</template>
+
+<script setup>
+import { gsap } from 'gsap';
+import SplitType from 'split-type'
+
+
+onMounted(() => {
+    const myText = new SplitType('#my-text')
+    gsap.from('.char', {
+        y: 100,
+        stagger: 0.07,
+        delay: 0.6,
+        duration: 0.6,
+    })
+})
+</script>
+
+<style scoped lang="scss">
+section {
+    display: grid;
+    place-content: center;
+    height: 100vh;
+}
+
+h1 {
+    font-size: 5rem;
+    line-height: 1;
+    text-transform: uppercase;
+    font-family: 'Bebas Neue';
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+    line-height: 5.9rem;
+}
+
+.bussines {
+    font-size: 3rem;
+    text-transform: uppercase;
+    font-family: 'Bebas Neue';
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+    line-height: 5.9rem;
+}
+
+.char {
+    transform: translateY(115px);
+    transition: transform .5s;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,10 @@
 <template>
     <div>
-        <section class="flex justify-center">
-            <div class="container">
-                <h1 class="split">Oktay</h1>
-                <h1 class="split2">Kaypak</h1>
-                <h1 class="split work">UI & UX Developer</h1>
-            </div>
-        </section>
-        <section style="height: 2000px;">
-
-        </section>
+        <HeaderSection />
     </div>
 </template>
 
 <script setup>
-import { gsap } from 'gsap';
-import { Observer } from 'gsap/Observer';
-import { Draggable } from 'gsap/Draggable';
-
-gsap.registerPlugin(Observer);
-
-onMounted(() => {
-    function rotate(value) {
-        gsap.to('.split', {
-            rotate: value + '_short',
-            ease: 'back.out',
-            duration: 0.5,
-            overwrite: true,
-            transformOrigin: 'center center',
-        }),
-            gsap.to('.split2', {
-                rotate: -value + '_short',
-                ease: 'back.out',
-                duration: 0.5,
-                overwrite: true,
-                transformOrigin: 'center center',
-            })
-    }
-    Observer.create({
-        target: window,
-        types: "wheel.touch.scroll.pointer",
-        onUp: () => { rotate('0') },
-        onDown: () => { rotate('180') },
-        onChange: (self) => {
-            if (self.direction === 1) {
-                rotate('180')
-            } else {
-                rotate('0')
-            }
-        }
-    })
-
-})
-
-
+import HeaderSection from '@/components/components/HeaderSection.vue'
 </script>
 
-<style scoped lang="scss">
-:root {
-    --text-color: #111;
-    --bg-color: #b9b3a9;
-}
-
-
-.container {
-    font-size: 5rem;
-    line-height: 1;
-    font-weight: 800;
-    margin-bottom: 1rem;
-    position: absolute;
-    top: 15vw;
-    left: 10vw;
-    z-index: 4;
-
-    @media (max-width: 768px) {
-        font-size: 16vw;
-    }
-
-
-}
-</style>
